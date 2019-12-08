58d5c96900046849c3df72da944a85120fd219df
XavierF-C <xavier_f-c@hotmail.com>
2019-12-07 21:01:43 -0500

    Ajout du module shader

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+#[macro_use]
 extern crate glium;
-use glium::{glutin, Surface};// Surface est un trait et doit être importé
+use glium::{glutin, Surface}; // Surface est un trait et doit être importé
+
+mod shader; // Permet de construire les shaders nécéssaires
 
 fn main() {
 
@@ -14,10 +17,14 @@ fn main() {
             parametres_fenetre,
             contexte_opengl,
             &boucle_evenements
-    ).unwrap();
+        ).unwrap();
     
-    let debut_programme = std::time::SystemTime::now();
+    // À garder?
+    //let debut_programme = std::time::SystemTime::now();
 
+    let programme_opengl = shader::ProgrammeOpenGL::new(&affichage);
+
+    // Cette closure représente la boucle principale du programme
     boucle_evenements.run(move |evenement, _, flot_controle| {
 
         match evenement {
@@ -28,31 +35,25 @@ fn main() {
                 *flot_controle = glutin::event_loop::ControlFlow::Exit;
                 return;
             },
-
+            
+            // On peut procéder à l'affichage dans ces scénarios
             glutin::event::Event::NewEvents(glutin::event::StartCause::ResumeTimeReached{..}) => (),
             glutin::event::Event::NewEvents(glutin::event::StartCause::Init) => (),
-            // On peut procéder à l'affichage dans ces scénarios
 
-            _ => return
+            // Sinon, on ignore l'évènement
+            _ => return 
         }
-        // Si l'évènement est de type NewEvents(StartCause) où StartCause = {ResumeTimeReached, Init},
-        // on peut rafraîchir l'affichage
-
+        
         // Permet de redéclencher la boucle pour la prochaine fois
         let prochaine_date_affichage = std::time::Instant::now() +
             std::time::Duration::from_nanos(16_666_667); // 10^9/60
         *flot_controle = glutin::event_loop::ControlFlow::WaitUntil(prochaine_date_affichage);
 
+
         // affichage.draw() retourne un struct Frame, sur lequel on peut dessiner 
         let mut cadre = affichage.draw();
-
-        let difference_temps = debut_programme.elapsed().unwrap().as_millis();
-        let periode = 0x7FF;
-        let proportion = (difference_temps & periode) as f32 / periode as f32;
-        println!("{}", proportion);
-        let rouge = proportion * (1.0 - proportion);
         
-        cadre.clear_color(rouge, 0.0, 1.0, 1.0);
+        cadre.clear_color(0.3, 0.3, 0.5, 1.0);
         //cadre.draw(); À compléter dès la création des shaders
         cadre.finish().unwrap();
     });
--- src/shader.rs
+++ src/shader.rs
@@ -1,3 +1,105 @@
-extern crate glium;
-use glium::{glutin, Surface};// Surface est un trait et doit être importé
+/**
+    Interface publique du module shader
+*/
 
+
+// Représente un programme de shaders OpenGL
+pub struct ProgrammeOpenGL {
+
+    vertex_shader: std::string::String,
+    fragment_shader: std::string::String,
+
+    pub programme: glium::Program,
+}
+
+impl ProgrammeOpenGL {
+
+    pub fn new(affichage: &glium::Display) -> ProgrammeOpenGL {
+
+        let vertex_shader = obtenir_vertex_shader();
+        let fragment_shader = obtenir_fragment_shader();
+        let programme = glium::Program::from_source(
+            affichage,
+            &vertex_shader,
+            &fragment_shader,
+            None);
+
+        // On vérifie si le programme est correct, sinon on arrête le programme avec l'erreur
+        let programme = match programme {
+
+            Ok(o) => o,
+            Err(e) => match e {
+                glium::program::ProgramCreationError::CompilationError(e) => {
+
+                    println!("\n\nIl y a au moins une erreur de compilation des shaders:\n{}", e);
+                    panic!("La compilation des shaders a échouée");
+                },
+
+                glium::program::ProgramCreationError::LinkingError(e) => {
+
+                    println!("\n\nIl y a au moins une erreur de linking des shaders:\n{}", e);
+                    panic!("La compilation des shaders a échouée");
+                },
+
+                _ => { 
+                    println!("\n\nUne erreur inconnue est survenue à la compilation des shaders.");
+                    println!("Voir glium::program::ProgramCreationError\n");
+                    panic!("La compilation des shaders a échouée")
+                },
+            }
+        };
+
+        ProgrammeOpenGL {
+            vertex_shader: vertex_shader,
+            fragment_shader: fragment_shader,
+            programme: programme 
+        }
+    }
+}
+
+
+
+
+
+/**
+    Partie privée du module shader 
+*/
+
+
+// Primitive pour OpenGL
+#[derive(Copy, Clone)]
+struct Sommet {
+    position: [f32; 3],
+}
+// Permet à Glium de l'utiliser avec OpenGL
+implement_vertex!(Sommet, position);
+
+
+// Déclaration de tous les shaders utilisés
+// La notation r#""# permet de préserver la chaîne brute
+
+fn obtenir_vertex_shader() -> std::string::String
+{
+    std::string::String::from(r#"
+        #version 330
+
+        in vec3 position;
+
+        void main() {
+            gl_Position = vec4(position, 1.0);
+        }
+    "#)
+}
+
+fn obtenir_fragment_shader() -> std::string::String
+{
+    std::string::String::from(r#"
+        #version 330
+        
+        out vec4 couleur;
+        
+        void main() {
+            couleur = vec4(0.5, 0.5, 0.5, 1.0);
+        }
+    "#)
+}
